779f2da02becb39b822fe718a1e7149b7efbb4bc
Alexander Hermes <alexander.hermes@grasshopperasia.com>
2019-12-09 16:26:20 +0800

    Fix numbering of challenges in set 2

--- set2.go
+++ set2.go
@@ -5,12 +5,12 @@ import "fmt"
 // Set2 solutions
 func Set2() {
 	//Challenge 1
-	C1S2()
+	C9()
 }
 
-// C1S2 solutions
-func C1S2() {
+// C9 solutions
+func C9() {
 	fmt.Println("---------------------- c1 ------------------------")
 	const trial = "YELLOW SUBMARINE"
-	fmt.Printf("Trial %v padded to 20: %q", trial, string(PadPKCS7([]byte(trial), 20)))
+	fmt.Printf("Trial %v padded to 20: %q\n", trial, string(PadPKCS7([]byte(trial), 20)))
 }
