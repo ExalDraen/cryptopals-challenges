1372a15a70896d98b15ce9828bd329d67e8e6a86
Alexander Hermes <alexander.hermes@grasshopperasia.com>
2020-01-29 17:11:34 +0800

    Reimplement ECB according to cipher.BlockMode

---
+++ ecb.go
@@ -0,0 +1,83 @@
+package main
+
+import "crypto/cipher"
+
+type ecb struct {
+	b         cipher.Block
+	blockSize int
+}
+
+type ecbDecrypter ecb
+type ecbEncrypter ecb
+
+func newECB(b cipher.Block) *ecb {
+	return &ecb{
+		b:         b,
+		blockSize: b.BlockSize(),
+	}
+}
+
+// NewECBDecrypter returns an implementation of ECB decryption for
+// a given block cipher
+func NewECBDecrypter(block cipher.Block) cipher.BlockMode {
+	return (*ecbDecrypter)(newECB(block))
+}
+
+// NewECBEncrypter returns an implementation of ECB encryption for
+// a given block cipher
+func NewECBEncrypter(block cipher.Block) cipher.BlockMode {
+	return (*ecbEncrypter)(newECB(block))
+}
+
+// BlockSize returns the block size of the cbc block cipher
+func (c *ecbDecrypter) BlockSize() int {
+	return c.blockSize
+}
+
+// BlockSize returns the block size of the cbc block cipher
+func (c *ecbEncrypter) BlockSize() int {
+	return c.blockSize
+}
+
+// CryptBlocks decrypts a number of blocks. The length of
+// src must be a multiple of the block size. Dst and src must overlap
+// entirely or not at all.
+func (c *ecbDecrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%c.blockSize != 0 {
+		panic("input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("output smaller than input")
+	}
+	srcBlocks := ChunkBytes(src, c.b.BlockSize())
+	dstBlocks := make([][]byte, len(srcBlocks))
+
+	for i := range srcBlocks {
+		dstBlocks[i] = make([]byte, c.blockSize)
+		c.b.Decrypt(dstBlocks[i], srcBlocks[i])
+	}
+	//recombine and update dst block
+	copy(dst, UnchunkBytes(dstBlocks))
+
+}
+
+// CryptBlocks encrypts a number of blocks. The length of
+// src must be a multiple of the block size. Dst and src must overlap
+// entirely or not at all.
+func (c *ecbEncrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%c.blockSize != 0 {
+		panic("input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("output smaller than input")
+	}
+	srcBlocks := ChunkBytes(src, c.b.BlockSize())
+	dstBlocks := make([][]byte, len(srcBlocks))
+
+	for i := range srcBlocks {
+		dstBlocks[i] = make([]byte, c.blockSize)
+		c.b.Encrypt(dstBlocks[i], srcBlocks[i])
+	}
+	//recombine and update dst block
+	copy(dst, UnchunkBytes(dstBlocks))
+}
---
+++ ecb_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"log"
+	"testing"
+)
+
+func TestEncryptDecryptECB(t *testing.T) {
+	ex := []struct {
+		key   []byte
+		input []byte
+	}{
+		{[]byte("Please test me!!"), []byte("We all live in a yellow submarine, yellow subm..")},
+		{[]byte("Dont Stop Me Now"), []byte("Cause we're having a good time..")},
+		{[]byte("YELLOW SUBMARINEYELLOW SUBMARINE"), []byte("YELLOW SUBMARINE")},
+	}
+
+	for _, e := range ex {
+		cypher, err := aes.NewCipher([]byte(e.key))
+		if err != nil {
+			log.Fatalf("failed to instantiate cypher with key %v: %v", e.key, err)
+		}
+		encrypter := NewECBEncrypter(cypher)
+		decrypter := NewECBDecrypter(cypher)
+		result := make([]byte, len(e.input))
+		encrypter.CryptBlocks(result, e.input)
+		decrypter.CryptBlocks(result, result)
+		if !bytes.Equal(result, e.input) {
+			t.Errorf("Encrypt-decrypt (key: %v) failed: \nInp: %v \nGot: %v", e.key, e.input, result)
+		}
+	}
+}
